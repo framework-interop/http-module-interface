03ed4f42972d56335cb881e52ba521e6b622aeda
David Négrier <d.negrier@thecodingmachine.com>
2015-01-29 18:23:10 +0100

    Fixing PHP-doc

--- src/HttpModuleInterface.php
+++ src/HttpModuleInterface.php
@@ -3,13 +3,17 @@ namespace Interop\Framework;
 
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
+/**
+ *  This interface as a base building-block to build framework-agnostic modules that provide
+ *  a HTTP middleware (see http://stackphp.com/ for more information about middlewares)
+ */
 interface HttpModuleInterface extends ModuleInterface
 {
 	/**
-	 * You can return a StackPHP middleware if the module provides one.
+	 * You should return a StackPHP middleware.
 	 *
 	 * @param $app HttpKernelInterface The kernel your middleware will be wrapping.
-	 * @return HttpKernelInterface|null
+	 * @return HttpKernelInterface
 	 */
 	public function getHttpMiddleware(HttpKernelInterface $app);
 }
